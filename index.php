38ece72add69dae3afe756af538c26801bb9a695
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-08 16:10:28 +0000

    Add editorconfig and reformat files

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-  require __DIR__ . "/vendor/autoload.php";
+	require __DIR__ . "/vendor/autoload.php";
 
-  $timer = new PHP_Timer();
+	$timer = new PHP_Timer();
 
-  $timer->start();
-  sleep(rand(1, 3));
-  $timer->stop();
-  print $timer->resourceUsage() . "\n";
+	$timer->start();
+	sleep(rand(1, 3));
+	$timer->stop();
+	print $timer->resourceUsage() . "\n";
