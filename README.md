e82bc42f7bacf549f20a55a1b3aff87979be3004
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-08 18:55:41 +0000

    Add WordPress Coding Standards

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # php-sandbox
+
+### Set default standard
+
+Run the following command to set the default coding standard:
+
+```
+./vendor/bin/phpcs --config-set default_standard WordPress
+```
--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
-	require __DIR__ . "/vendor/autoload.php";
+	/**
+	 * Quick autoload test
+	 *
+	 * @link:    https://github.com/thisishuey/php-sandbox
+	 * @license: GPL-2.0-or-later
+	 */
+	require __DIR__ . '/vendor/autoload.php';
 
 	$timer = new PHP_Timer();
 
 	$timer->start();
-	sleep(rand(1, 3));
+	sleep( wp_rand( 1, 3 ) );
 	$timer->stop();
 	print $timer->resourceUsage() . "\n";
